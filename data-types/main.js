1d17d1b82e09b3cccdec3be25fa1ff56d6d86e3c
Marisha-tech <51979512+Marisha-tech@users.noreply.github.com>
2021-04-25 21:50:02 +0700

    Типы данных. Map и Set

--- data-types/main.js
+++ data-types/main.js
@@ -1,271 +1,84 @@
-// https://learn.javascript.ru/array-methods
-// Типы данных. Методы массивов
+// https://learn.javascript.ru/map-set
+// Типы данных. Map и Set
+
+
+// Map – это коллекция ключ/значение, как и Object. Но основное отличие в том, что Map позволяет использовать ключи любого типа.
+
 
 // #1 ---------------------------------------------------------------------------
-/* 
-// Переведите текст вида border-left-width в borderLeftWidth
-// Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
-// То есть дефисы удаляются, а все слова после них получают заглавную букву.
-// Примеры:
-// camelize("background-color") == 'backgroundColor';
-// camelize("list-style-image") == 'listStyleImage';
-// camelize("-webkit-transition") == 'WebkitTransition';
-// P.S. Подсказка: используйте split, чтобы разбить строку на массив символов, потом переделайте всё как нужно и методом join соедините обратно.
-
-function camelize(str) {
-  return str
-    .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
-    .map(
-      // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
-      // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
-      (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
-    )
-    .join(''); // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
+/* // Фильтрация уникальных элементов массива
+// Допустим, у нас есть массив arr.
+// Создайте функцию unique(arr), которая вернёт массив уникальных, не повторяющихся значений массива arr.
+// P.S. Здесь мы используем строки, но значения могут быть любого типа.
+// P.P.S. Используйте Set для хранения уникальных значений.
+function unique(arr) {
+    // return [...new Set(arr)];
+    //ИЛИ
+    return Array.from(new Set(arr));
 }
-console.log(camelize("background-color")) ;
-console.log(camelize("list-style-image")) ;
-console.log(camelize("-webkit-transition")) ; */
+
+console.log(unique(["Hare", "Krishna", "Hare", "Krishna",
+"Krishna", "Krishna", "Hare", "Hare", ":-O"
+])); */
 
 
 // #2 ---------------------------------------------------------------------------
-/* // Фильтрация по диапазону
-// Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет в нём элементы между a и b и отдаёт массив этих элементов.
-// Функция должна возвращать новый массив и не изменять исходный.
+// Отфильтруйте анаграммы
+// Анаграммы – это слова, у которых те же буквы в том же количестве, но они располагаются в другом порядке.
+// Напишите функцию aclean(arr), которая возвращает массив слов, очищенный от анаграмм.
 // Например:
-// let arr = [5, 3, 8, 1];
-// let filtered = filterRange(arr, 1, 4);
-// alert( filtered ); // 3,1 (совпадающие значения)
-// alert( arr ); // 5,3,8,1 (без изменений)
- 
+// let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+// alert( aclean(arr) ); // "nap,teachers,ear" or "PAN,cheaters,era"
+// Из каждой группы анаграмм должно остаться только одно слово, не важно какое.
 
-// let arr = [5, 3, 8, 1]
-//   let a = 1
-//   let b = 4
-//   let newArr = []
+// let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
 
-// function filterRange(arr, a, b) {
-//   for(let i = 0; i < arr.length; i++) {
-//     if(arr[i] >= a && arr[i] <=b) {
-//       newArr.push(i)
-//     }
-//   }
-//   console.log(newArr)
-//   return arr
-// }
-// console.log(filterRange(arr, a, b))
+/* function aclean(arr){
+  // Чтобы найти все анаграммы, давайте разобьём каждое слово на буквы и отсортируем их, а потом объединим получившийся массив снова в строку. После этого все анаграммы будут одинаковы.
+  let map = new Map()
 
-function filterRange(arr, a, b) {
-  // добавлены скобки вокруг выражения для улучшения читабельности
-  return arr.filter(item => (a <= item && item <= b));
-}
-
-let arr = [5, 3, 8, 1];
-
-let filtered = filterRange(arr, 1, 4);
-
-alert( filtered ); // 3,1 (совпадающие значения)
-
-alert( arr ); // 5,3,8,1 (без изменений) */
-
-
-
-// #3 ---------------------------------------------------------------------------
-/* // Фильтрация по диапазону "на месте"
-// Напишите функцию filterRangeInPlace(arr, a, b), которая принимает массив arr и удаляет из него все значения кроме тех, которые находятся между a и b. То есть, проверка имеет вид a ≤ arr[i] ≤ b.
-// Функция должна изменять принимаемый массив и ничего не возвращать.
-// Например:
-// let arr = [5, 3, 8, 1];
-// filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
-// alert( arr ); // [3, 1]
-
-function filterRangeInPlace(arr, a, b) {
-
-  for (let i = 0; i < arr.length; i++) {
-    let val = arr[i];
-
-    // удалить, если за пределами интервала
-    if (val < a || val > b) {
-      arr.splice(i, 1);
-      i--;
-    }
+  for (let word of arr) {
+    // разбиваем слово на буквы, сортируем и объединяем снова в строку
+    let sorted = word.toLowerCase().split('').sort().join('') // (*)
+    map.set(sorted, word)
   }
 
-}
+  // Строка с отсортированными буквами получается в результате цепочки вызовов в строке (*)
 
-let arr = [5, 3, 8, 1];
+  // Для удобства, давайте разделим это на несколько строк
+  return Array.from(map.values())
+  let sorted = arr[i] // PAN
+    .toLowerCase() // pan
+    .split('') // ["p","a","n"]
+    .sort() // ["a","n","p"]
+    .join('') // anp
+  // Два разных слова 'PAN' и 'nap' принимают ту же самую форму после сортировки букв – 'anp'
 
-filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+  // Следующая линия помещает слово в объект Map:
 
-alert( arr ); // [3, 1] */
+  map.set(sorted, word)
+  // Если мы когда-либо ещё встретим слово в той же отсортированной форме, тогда это слово перезапишет значение с тем же ключом в объекте. Таким образом, нескольким словам у нас будет всегда соответствовать одна отсортированная форма.
+  // В конце Array.from(map.values()) принимает итерируемый объект значений объекта Map (в данном случае нам не нужны ключи) и возвращает их в виде массива.
+  // Также в этом случае вместо Map мы можем использовать простой объект, потому что ключи являются строками.
 
+} */
 
-
-// #4 ---------------------------------------------------------------------------
-/* // Сортировать в порядке по убыванию
-// let arr = [5, 2, 1, -10, 8];
-// // ... ваш код для сортировки по убыванию
-// alert( arr ); // 8, 5, 2, 1, -10
-
-// function compareNumeric(a, b) {
-//   if (a < b) return 1;
-//   if (a == b) return 0;
-//   if (a > b) return -1;
-// }
-
-// let arr = [ 5, 2, 1, -10, 8 ];
-
-// arr.sort(compareNumeric);
-
-// alert(arr); // 8, 5, 2, 1, -10
-
-let arr = [5, 2, 1, -10, 8];
-
-arr.sort((a, b) => b - a);
-
-alert( arr ); */
+// console.log(aclean(arr))
 
 
-
-// #5 ---------------------------------------------------------------------------
-/* // Скопировать и отсортировать массив
-// У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
-// Создайте функцию copySorted(arr), которая будет возвращать такую копию.
-
-function copySorted(arr) {
+// ИЛИ
+function aclean(arr) {
+    let obj = {};
   
-  return arr.slice().sort(); //сделать независимую копию массива, то нужно использовать метод slice без аргументов.
-}
-
-let arr = ["HTML", "JavaScript", "CSS"];
-let sorted = copySorted(arr);
-alert(arr + ' - ' + ' arr')
-alert(sorted) */
-
-
-
-// #6 ---------------------------------------------------------------------------
-/* // Создать расширяемый калькулятор
-// Создайте функцию конструктор Calculator, которая создаёт «расширяемые» объекты калькулятора.
-// Задание состоит из двух частей.
-// Во-первых, реализуйте метод calculate(str), который принимает строку типа "1 + 2" в формате «ЧИСЛО оператор ЧИСЛО» (разделено пробелами) и возвращает результат. Метод должен понимать плюс + и минус -.
-// Пример использования:
-// let calc = new Calculator;
-// alert( calc.calculate("3 + 7") ); // 10
-// Затем добавьте метод addMethod(name, func), который добавляет в калькулятор новые операции. Он принимает оператор name и функцию с двумя аргументами func(a,b), которая описывает его.
-// Например, давайте добавим умножение *, деление / и возведение в степень **:
-// let powerCalc = new Calculator;
-// powerCalc.addMethod("*", (a, b) => a * b);
-// powerCalc.addMethod("/", (a, b) => a / b);
-// powerCalc.addMethod("**", (a, b) => a ** b);
-// let result = powerCalc.calculate("2 ** 3");
-// alert( result ); // 8
-// Для этой задачи не нужны скобки или сложные выражения.
-// Числа и оператор разделены ровно одним пробелом.
-// Не лишним будет добавить обработку ошибок.
-
-
-let number1
-let operator
-let number2
-let sum
-let str = "1 + 2"
-
-function Calculator() {
-
-  //Обратите внимание, как хранятся методы. Они просто добавляются к внутреннему объекту.
-  this.methods = {
-    '-': (a, b) => a - b,
-    '+': (a, b) => a + b,
-  }
-
-  //Все тесты и числовые преобразования выполняются в методе calculate. В будущем он может быть расширен для поддержки более сложных выражений.
-  this.calculate = function(str) {
-
-    let split = str.split(' '),
-      a = +split[0],
-      op = split[1],
-      b = +split[2]
-
-    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
-      return NaN;
+    for (let i = 0; i < arr.length; i++) {
+      let sorted = arr[i].toLowerCase().split("").sort().join("");
+      obj[sorted] = arr[i];
     }
-
-    return this.methods[op](a, b);
-
+  
+    return Object.values(obj);
   }
-
-  this.addMethod = function(name, func) {
-    this.methods[name] = func;
-  };
-
-}
-let calc = new Calculator;
-alert( calc.calculate("3 + 7") ); // 10 */
-
-
-
-// #7 ---------------------------------------------------------------------------
-/* // Трансформировать в массив имён
-// У вас есть массив объектов user, и в каждом из них есть user.name. Напишите код, который преобразует их в массив имён.
-
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
-
-let users = [ vasya, petya, masha ];
-
-let names = users.map(item => item.name);
-
-alert( names ); // Вася, Петя, Маша */
-
-
-
-// #8 ---------------------------------------------------------------------------
-/* // Трансформировать в объекты
-// У вас есть массив объектов user, и у каждого из объектов есть name, surname и id.
-// Напишите код, который создаст ещё один массив объектов с параметрами id и fullName, где fullName – состоит из name и surname.
-let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
-let petya = { name: "Петя", surname: "Иванов", id: 2 };
-let masha = { name: "Маша", surname: "Петрова", id: 3 };
-
-let users = [ vasya, petya, masha ];
-
-let usersMapped = users.map(user => ({
-  fullName: `${user.name} ${user.surname}`,
-  id: user.id
-}));
-console.log(usersMapped)
-
-alert( usersMapped[0].id ) // 1
-alert( usersMapped[0].fullName ) // Вася Пупкин */
-
-
-
-
-// #9 ---------------------------------------------------------------------------
-// Отсортировать пользователей по возрасту
-// Напишите функцию sortByAge(users), которая принимает массив объектов со свойством age и сортирует их по нему.
-
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
-
-let users = [ vasya, petya, masha ];
-
-function sortByAge(users) {
-  users.sort( ({age: a}, {age: b}) => a - b );
-  // console.log(users)
-}
-
-
-// теперь отсортировано: [vasya, masha, petya]
-// alert(users[0].name); // Вася
-// alert(users[1].name); // Маша
-// alert(users[2].name); // Петя
-// #10 ---------------------------------------------------------------------------
-
-
-// #11 ---------------------------------------------------------------------------
-
-
-// #12 ---------------------------------------------------------------------------
+  
+  let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+  
+  alert( aclean(arr) );
+// #3 ---------------------------------------------------------------------------
