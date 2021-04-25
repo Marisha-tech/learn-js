9256e70797b40971132d5891a6d2fb66dd2cd289
Marisha-tech <51979512+Marisha-tech@users.noreply.github.com>
2021-04-25 16:29:14 +0700

    Типы данных. Методы массивов

---
+++ data-types/5-lesson.js
@@ -0,0 +1,308 @@
+// https://learn.javascript.ru/array-methods
+// Типы данных. Методы массивов
+
+// #1 ---------------------------------------------------------------------------
+/* 
+// Переведите текст вида border-left-width в borderLeftWidth
+// Напишите функцию camelize(str), которая преобразует строки вида «my-short-string» в «myShortString».
+// То есть дефисы удаляются, а все слова после них получают заглавную букву.
+// Примеры:
+// camelize("background-color") == 'backgroundColor';
+// camelize("list-style-image") == 'listStyleImage';
+// camelize("-webkit-transition") == 'WebkitTransition';
+// P.S. Подсказка: используйте split, чтобы разбить строку на массив символов, потом переделайте всё как нужно и методом join соедините обратно.
+
+function camelize(str) {
+  return str
+    .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
+    .map(
+      // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
+      // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
+      (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(''); // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
+}
+console.log(camelize("background-color")) ;
+console.log(camelize("list-style-image")) ;
+console.log(camelize("-webkit-transition")) ; */
+
+// #2 ---------------------------------------------------------------------------
+/* // Фильтрация по диапазону
+// Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет в нём элементы между a и b и отдаёт массив этих элементов.
+// Функция должна возвращать новый массив и не изменять исходный.
+// Например:
+// let arr = [5, 3, 8, 1];
+// let filtered = filterRange(arr, 1, 4);
+// alert( filtered ); // 3,1 (совпадающие значения)
+// alert( arr ); // 5,3,8,1 (без изменений)
+ 
+
+// let arr = [5, 3, 8, 1]
+//   let a = 1
+//   let b = 4
+//   let newArr = []
+
+// function filterRange(arr, a, b) {
+//   for(let i = 0; i < arr.length; i++) {
+//     if(arr[i] >= a && arr[i] <=b) {
+//       newArr.push(i)
+//     }
+//   }
+//   console.log(newArr)
+//   return arr
+// }
+// console.log(filterRange(arr, a, b))
+
+function filterRange(arr, a, b) {
+  // добавлены скобки вокруг выражения для улучшения читабельности
+  return arr.filter(item => (a <= item && item <= b));
+}
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (совпадающие значения)
+
+alert( arr ); // 5,3,8,1 (без изменений) */
+
+// #3 ---------------------------------------------------------------------------
+/* // Фильтрация по диапазону "на месте"
+// Напишите функцию filterRangeInPlace(arr, a, b), которая принимает массив arr и удаляет из него все значения кроме тех, которые находятся между a и b. То есть, проверка имеет вид a ≤ arr[i] ≤ b.
+// Функция должна изменять принимаемый массив и ничего не возвращать.
+// Например:
+// let arr = [5, 3, 8, 1];
+// filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+// alert( arr ); // [3, 1]
+
+function filterRangeInPlace(arr, a, b) {
+
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+
+    // удалить, если за пределами интервала
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+
+}
+
+let arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+
+alert( arr ); // [3, 1] */
+
+// #4 ---------------------------------------------------------------------------
+/* // Сортировать в порядке по убыванию
+// let arr = [5, 2, 1, -10, 8];
+// // ... ваш код для сортировки по убыванию
+// alert( arr ); // 8, 5, 2, 1, -10
+
+// function compareNumeric(a, b) {
+//   if (a < b) return 1;
+//   if (a == b) return 0;
+//   if (a > b) return -1;
+// }
+
+// let arr = [ 5, 2, 1, -10, 8 ];
+
+// arr.sort(compareNumeric);
+
+// alert(arr); // 8, 5, 2, 1, -10
+
+let arr = [5, 2, 1, -10, 8];
+
+arr.sort((a, b) => b - a);
+
+alert( arr ); */
+
+// #5 ---------------------------------------------------------------------------
+/* // Скопировать и отсортировать массив
+// У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
+// Создайте функцию copySorted(arr), которая будет возвращать такую копию.
+
+function copySorted(arr) {
+  
+  return arr.slice().sort(); //сделать независимую копию массива, то нужно использовать метод slice без аргументов.
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+let sorted = copySorted(arr);
+alert(arr + ' - ' + ' arr')
+alert(sorted) */
+
+// #6 ---------------------------------------------------------------------------
+/* // Создать расширяемый калькулятор
+// Создайте функцию конструктор Calculator, которая создаёт «расширяемые» объекты калькулятора.
+// Задание состоит из двух частей.
+// Во-первых, реализуйте метод calculate(str), который принимает строку типа "1 + 2" в формате «ЧИСЛО оператор ЧИСЛО» (разделено пробелами) и возвращает результат. Метод должен понимать плюс + и минус -.
+// Пример использования:
+// let calc = new Calculator;
+// alert( calc.calculate("3 + 7") ); // 10
+// Затем добавьте метод addMethod(name, func), который добавляет в калькулятор новые операции. Он принимает оператор name и функцию с двумя аргументами func(a,b), которая описывает его.
+// Например, давайте добавим умножение *, деление / и возведение в степень **:
+// let powerCalc = new Calculator;
+// powerCalc.addMethod("*", (a, b) => a * b);
+// powerCalc.addMethod("/", (a, b) => a / b);
+// powerCalc.addMethod("**", (a, b) => a ** b);
+// let result = powerCalc.calculate("2 ** 3");
+// alert( result ); // 8
+// Для этой задачи не нужны скобки или сложные выражения.
+// Числа и оператор разделены ровно одним пробелом.
+// Не лишним будет добавить обработку ошибок.
+
+
+let number1
+let operator
+let number2
+let sum
+let str = "1 + 2"
+
+function Calculator() {
+
+  //Обратите внимание, как хранятся методы. Они просто добавляются к внутреннему объекту.
+  this.methods = {
+    '-': (a, b) => a - b,
+    '+': (a, b) => a + b,
+  }
+
+  //Все тесты и числовые преобразования выполняются в методе calculate. В будущем он может быть расширен для поддержки более сложных выражений.
+  this.calculate = function(str) {
+
+    let split = str.split(' '),
+      a = +split[0],
+      op = split[1],
+      b = +split[2]
+
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
+    }
+
+    return this.methods[op](a, b);
+
+  }
+
+  this.addMethod = function(name, func) {
+    this.methods[name] = func;
+  };
+
+}
+let calc = new Calculator;
+alert( calc.calculate("3 + 7") ); // 10 */
+
+// #7 ---------------------------------------------------------------------------
+/* // Трансформировать в массив имён
+// У вас есть массив объектов user, и в каждом из них есть user.name. Напишите код, который преобразует их в массив имён.
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = users.map(item => item.name);
+
+alert( names ); // Вася, Петя, Маша */
+
+// #8 ---------------------------------------------------------------------------
+/* // Трансформировать в объекты
+// У вас есть массив объектов user, и у каждого из объектов есть name, surname и id.
+// Напишите код, который создаст ещё один массив объектов с параметрами id и fullName, где fullName – состоит из name и surname.
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users = [ vasya, petya, masha ];
+
+let usersMapped = users.map(user => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id
+}));
+console.log(usersMapped)
+
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // Вася Пупкин */
+
+// #9 ---------------------------------------------------------------------------
+/* // Отсортировать пользователей по возрасту
+// Напишите функцию sortByAge(users), которая принимает массив объектов со свойством age и сортирует их по нему.
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+function sortByAge(users) {
+  users.sort( ({age: a}, {age: b}) => a - b );
+  // console.log(users)
+}
+
+// теперь отсортировано: [vasya, masha, petya]
+// alert(users[0].name); // Вася
+// alert(users[1].name); // Маша
+// alert(users[2].name); // Петя */
+
+// #10 ---------------------------------------------------------------------------
+
+// Перемешайте массив
+// Напишите функцию shuffle(array), которая перемешивает (переупорядочивает случайным образом) элементы массива.
+// Многократные прогоны через shuffle могут привести к разным последовательностям элементов.
+// Например:
+// let arr = [1, 2, 3];
+// shuffle(arr);
+// // arr = [3, 2, 1]
+// shuffle(arr);
+// // arr = [2, 1, 3]
+// shuffle(arr);
+// // arr = [3, 1, 2]
+// // ...
+// Все последовательности элементов должны иметь одинаковую вероятность. Например, [1,2,3] может быть переупорядочено как [1,2,3] или [1,3,2], или [3,1,2] и т.д., с равной вероятностью каждого случая.
+
+// let array = [1, 2, 3]
+
+// function shuffle(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+
+//     // поменять элементы местами
+//     // мы используем для этого синтаксис "деструктурирующее присваивание"
+//     // подробнее о нём - в следующих главах
+//     // то же самое можно записать как:
+//     // let t = array[i]; array[i] = array[j]; array[j] = t
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+// }
+
+// shuffle(array)
+
+// #11 ---------------------------------------------------------------------------
+
+// Оставить уникальные элементы массива
+// Пусть arr – массив строк.
+// Напишите функцию unique(arr), которая возвращает массив, содержащий только уникальные элементы arr.
+
+function unique(arr) {
+  /* ваш код */
+  let result = [];
+
+  for (let str of arr) {
+    if (!result.includes(str)) {
+      result.push(str);
+    }
+  }
+
+  return result;
+}
+
+let strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", ":-O"
+];
+
+alert( unique(strings) ); // кришна, харе, :-O
+
+// #12 ---------------------------------------------------------------------------
+
